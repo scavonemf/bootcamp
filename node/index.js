d4a003bb4fb693fcc0d7f9718d5c68ac7ca9f1cd
mflir <scavone.mf@gmail.com>
2021-03-23 20:29:38 -0300

    add express and routes

--- node/index.js
+++ node/index.js
@@ -1,4 +1,9 @@
-const http = require('http') //common js
+// const http = require('http') //common js
+
+const { request, response } = require('express')
+const express = require('express')
+
+const app = express()
 
 const notes = [
   {
@@ -27,12 +32,52 @@ const notes = [
   },
 ]
 
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-type': 'aplication/json' })
-  // response.end('Hello Word') on localhost:3001
-  response.end(JSON.stringify(notes))
+// const app = http.createServer((request, response) => {
+//   response.writeHead(200, { 'Content-type': 'aplication/json' })
+//   // response.end('Hello Word') on localhost:3001
+//   response.end(JSON.stringify(notes))
+// })
+
+app.get('/', (request, response) => {
+  response.send('<h1>hello world </h1>')
 })
 
+app.get('/api/notes', (request, response) => {
+  response.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  // response.send(id)
+  const note = notes.find(note => note.id === id)
+
+
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+  console.log({ note })
+
+})
+
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
+  response.status(204).end()
+})
+
+app.post('/api/notes/:id', (request, response) => {
+  const note = request.body
+  console.log(note)
+
+  response.json(note)
+})
+
+
+
 const PORT = 3001 //the port have to be free
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`) //on visual studio
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`) //on visual studio
+})
